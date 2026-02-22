ef01a5f2640a3c940785edc0b450d702d2af8643
Tim Perdok <tim-perdok@hotmail.com>
2026-02-22 16:08:44 +0100

    Clean data

--- src/components/map/Popup.tsx
+++ src/components/map/Popup.tsx
@@ -1,12 +1,18 @@
-import { FlexRow, FlexColumn } from "@components/Flex";
+import { FlexColumn, FlexRow } from "@components/Flex";
 import { MarkerData } from "@localtypes/Map";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import styled from "styled-components";
+
+const Container = styled.div`
+    user-select: text;
+`
 
 export default function Popup({ markerData: marker, position }: { markerData: MarkerData, position: [number, number] }) {
     const { name, type } = marker;
     const toWiki = (name: string) => `https://wiki.projectgorgon.com/w/index.php?search=${name}`
     const roundedPosition = [position[0].toFixed(2), position[1].toFixed(2)] as [string, string];
-    return <FlexRow>
+    return <Container>
+        <FlexRow>
         <FlexColumn>
             <Typography variant="h6">{name}</Typography>
             <Typography variant="body1">{type}</Typography>
@@ -17,4 +23,5 @@ export default function Popup({ markerData: marker, position }: { markerData: Ma
             </Typography>
         </FlexColumn>
     </FlexRow>
+    </Container>
 }
